5c560f9a59806416a0096dacbe7b4e3bdbb8f0d8
nishatoma <nishatoma@gmail.com>
2019-02-21 16:48:24 -0800

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  myForm: FormGroup;
+  projectStatus: string[] = ['Stable', 'Critical', 'Finished'];
+  defaultProjectStatus: string = 'Stable';
+
+  ngOnInit() {
+
+    this.myForm = new FormGroup({
+      'project': new FormControl(null, [Validators.required, this.nameValidatorAsync.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl(this.defaultProjectStatus),
+    });
+
+  }
+
+  // Submit the form
+  submitForm() {
+    console.log(this.myForm);
+  }
+
+  nameValidator(control: FormControl): {[s: string]: true} {
+    if (control.value !== undefined && control.value === 'test') {
+      return {'nameWrong': true};
+    }
+    return null;
+  }
+
+  nameValidatorAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value !== undefined && control.value === 'test') {
+          resolve({'nameWrong': true});
+        } else {
+          resolve(null);
+        }
+      }, 1000)
+    });
+    return promise;
+  } 
 }
